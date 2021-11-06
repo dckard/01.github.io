f9ae7c33e5a0519da16a0d2d3440c719eb8a8d39
DckArd <dickyardian231@gmail.com>
2021-11-06 16:20:57 +0700

    Portfolio 01

---
+++ app.js
@@ -0,0 +1,492 @@
+let homeContent = `<div class="content-wrapper">
+<section class="home ">
+    <div class="greetings content-card">
+        <div class="greetings-title">
+            <h1 class="t1"> HI </h1>
+            <h1 class="t2"> My Name is <span>Dicky</span> </h1>
+            <h1 class="t3"> I am a Web Developer </h1>
+        </div>
+
+    </div>
+
+</section>`;
+
+let aboutContent = `<section class="about-wrapper">
+<div class="about-section about-content-card">
+  <div class="about-content">
+    <div class="about-title">
+      <h1>About</h1>
+    </div>
+    <div class="about-text">
+      <p>
+        My name is Dicky Ardian.<br />
+        In 2018 I decided to change my career. <br />
+        I wanted to learn about what I was really interested in and what I
+        enjoyed doing in my free time<br />
+        and so I began putting serious effort into creating websites.<br />
+        That same year, 2018, I quit my job and devoted myself completely to
+        studying and creating. <br />
+        <br />
+       
+        I began with <i class="fab fa-html5"></i> and
+        <i class="fab fa-css3-alt"></i> . I created a few landing pages. As
+        I have already worked in PhotoShop, PrmierPro, AfterEffects I
+        independently created mock-ups and turned them into code. <br />When
+        I felt ready to move on, I turned to
+        <i class="fab fa-js"></i> .<br />
+        <br />Then I realized that I needed someone to help me find the
+        right direction. <br />ComTrade had a Front End Development course,
+        with a strong focus on JS. I enrolled in it.<br />
+        In September 2019, I graduated with a grade 10.<br />
+        As I was studying, I also worked on JS algorithms on the
+        <i class="fab fa-free-code-camp"></i> and I created pages and small
+        application which are not complete as they have been constructed
+        with a specific idea and testing of new options. When I felt
+        comfortable working with JS i went on to learn
+        <i class="fab fa-react"></i>
+        <br />
+        As i went deeper into programming i wanted to do more than just front-end so i started learning <i class="fab fa-node"></i>, Express.js, MongoDB 
+      </p>
+    </div>
+  </div>
+</div>
+</section>`;
+
+let workContent = `
+
+        <div class="work-content work-content-card">
+            <div class="work-gallery">
+
+                <div class="work-project-card card-6">
+                    <h1 class="work-project-card-title tooltip ">
+                    Real Estate gold  <span class="tooltiptext">
+                    Project idea: 
+                    <br>-Responsive 
+                    <br>-Filter the objects 
+                    
+                    </span>
+                    </h1>
+                    <a href="https://dckard.github.io/" target="_blank">
+                    <img class="workPic tooltip" src="https://raw.githubusercontent.com/DjordjevicN/Real-Estate-gold/master/Screenshot_2.jpg"> </a>
+                    <a href="https://dckard.github.io/">  
+                    <p class="button-project btn-github ">Go to GITHUB </p></a>
+                </div>
+
+                <div class="work-project-card card-5">
+                    <h1 class="work-project-card-title tooltip">
+                        Digital Agency <span class="tooltiptext">
+                        Project idea: 
+                        <br>-Scroll Magic - animate on scroll
+                        <br>-GSAP 
+                        <br>-Video Background
+                        
+                        </span>
+                    </h1>
+                    <a href="https://djordjevicn.github.io/Digital-Agency/" target="_blank">
+                    <img class="workPic" src="https://raw.githubusercontent.com/DjordjevicN/Digital-Agency/master/Screenshot_2.jpg"> </a>
+                    <a href="https://github.com/DjordjevicN/Digital-Agency" target="_blank">  
+                    <p class="button-project btn-github ">Go to GITHUB</p>
+                    </a>
+                </div>
+
+                <div class="work-project-card card-4">
+                        <h1 class="work-project-card-title tooltip">
+                        Mountain Parallax <span class="tooltiptext">
+                        Project idea: 
+                        <br>-Responsive 
+                        <br>-Parallax 
+                        <br>-GSAP
+                        
+                        </span>
+                    </h1>
+                        <a href="https://djordjevicn.github.io/Parallax-practise-Mountin/" target="_blank">
+                    <img class="workPic" src="https://raw.githubusercontent.com/DjordjevicN/Parallax-practise-Mountin/master/Screenshot_1.jpg"> </a>
+                    <a href="https://github.com/DjordjevicN/Parallax-practise-Mountin" target="_blank">  
+                    <p class="button-project btn-github ">Go to GITHUB</p>
+                    </a>
+                </div>
+
+                <div class="work-project-card card-3">
+                    <h1 class="work-project-card-title tooltip">
+                        Shoe Shop <span class="tooltiptext">
+                        Project idea: 
+                        <br>-Bootstrap 
+                        <br>-Responsive
+                        <br>-Shop layout 
+                        
+                        </span>
+                    </h1>
+                    <a href="https://djordjevicn.github.io/tezga-shop-shoes/" target="_blank">
+                    <img class="workPic" src="https://raw.githubusercontent.com/DjordjevicN/tezga-shop-shoes/master/Screenshot_3.jpg"> </a>
+                    <a href="https://github.com/DjordjevicN/tezga-shop-shoes" target="_blank">  
+                    <p class="button-project btn-github ">Go to GITHUB</p>
+                    </a>
+                </div>
+
+                <div class="work-project-card card-2">
+                    <h1 class="work-project-card-title tooltip">
+                        Burger shop <span class="tooltiptext">
+                        School Project: 
+                        <br>-Burger Shop
+                        <br>-Shopping Cart
+                        <br>-Anime.js
+                        
+                        </span>
+                    </h1>
+                    <a href="https://djordjevicn.github.io/eCommerce-Burgelicious/" target="_blank">
+                    <img class="workPic" src="https://raw.githubusercontent.com/DjordjevicN/imagesRep/master/sc2.jpg"> </a>
+                    <a href="https://github.com/DjordjevicN/eCommerce-Burgelicious" target="_blank">  
+                    <p class="button-project btn-github ">Go to GITHUB</p>
+                    </a>
+                </div>
+
+                <div class="work-project-card card-2">
+                    <h1 class="work-project-card-title tooltip">
+                        Movie API search <span class="tooltiptext">
+                        Project idea: 
+                        <br>-Get information from API 
+                         
+                        
+                        </span>
+                    </h1>
+                    <a href="https://djordjevicn.github.io/Movie-Search-API-/" target="_blank">
+                    <img class="workPic" src="https://raw.githubusercontent.com/DjordjevicN/Movie-Search-API-/master/moviesearch.jpg"> </a>
+                    <a href="https://github.com/DjordjevicN/Movie-Search-API-" target="_blank">  
+                    <p class="button-project btn-github ">Go to GITHUB</p>
+                    </a>
+                </div>
+
+                
+            </div>
+            <p class="mobile">GO MOBILE</p>
+        </div>
+      
+  
+`;
+
+let skillsContent = `<div class="skills-section">
+<div class="skills-content skills-content-card">
+    <div class="skills-title">
+        <h1>Skills</h1>
+    </div>
+    <div class="skills-text">
+        <p> My main expertise is Front End Development</p>
+        <p> using<br/>- HTML, CSS, JavaScript, React</p>
+        <p>I also use:</p>
+        <p>- Node.js , Express ,<br/>
+         - MongoDB, Firebase</p>
+        <p>- React-Admin</p>
+
+        <p>- GSAP, Anime.js, ScrollMagic, SASS, <br/>
+        - Bootstrap , GIT, Firebase,<br/>
+         - PhotoShop, AfterEffects, PremierPro </p>
+
+    </div>
+</div>
+</div>`;
+
+let mobileContent = `<section class="work-wrapper">
+<div class="work-content work-content-card">
+    <div class="work-gallery">
+
+        <div class="work-project-card card-6">
+            <h1 class="work-project-card-title ">
+            Real Estate gold
+            </h1>
+            <a href="https://hopeful-einstein-2a0f32.netlify.com/" target="_blank">
+            <img class="qr-card" src="./QRprojects/realestateQR.jpg"> </a>
+            <a href="https://github.com/DjordjevicN/Real-Estate-gold">  
+            <p class="button-project btn-github ">Go to GITHUB</p></a>
+        </div>
+
+        <div class="work-project-card card-5">
+            <h1 class="work-project-card-title ">
+                Digital Agency
+            </h1>
+            <a href="https://djordjevicn.github.io/Digital-Agency/" target="_blank">
+            <img class="qr-card" src="./QRprojects/digitalagensyQR.jpg"> </a>
+            <a href="https://github.com/DjordjevicN/Digital-Agency" target="_blank">  
+            <p class="button-project btn-github ">Go to GITHUB</p>
+            </a>
+        </div>
+
+        <div class="work-project-card card-4">
+                <h1 class="work-project-card-title ">
+                Mountain Parallax
+            </h1>
+                <a href="https://djordjevicn.github.io/Parallax-practise-Mountin/" target="_blank">
+            <img class="qr-card" src="./QRprojects/paralaxMountinQR.jpg"> </a>
+            <a href="https://github.com/DjordjevicN/Parallax-practise-Mountin" target="_blank">  
+            <p class="button-project btn-github ">Go to GITHUB</p>
+            </a>
+        </div>
+
+        <div class="work-project-card card-3">
+            <h1 class="work-project-card-title ">
+                Shoe Shop
+            </h1>
+            <a href="https://djordjevicn.github.io/tezga-shop-shoes/" target="_blank">
+            <img class="qr-card" src="./QRprojects/tezgaShopQR.jpg"> </a>
+            <a href="https://github.com/DjordjevicN/tezga-shop-shoes" target="_blank">  
+            <p class="button-project btn-github ">Go to GITHUB</p>
+            </a>
+        </div>
+
+        <div class="work-project-card card-2">
+            <h1 class="work-project-card-title ">
+                Burger Online
+            </h1>
+            <a href="https://djordjevicn.github.io/eCommerce-Burgelicious/" target="_blank">
+            <img class="qr-card" src="./QRprojects/burgelicious.jpg"> </a>
+            <a href="https://github.com/DjordjevicN/eCommerce-Burgelicious" target="_blank">  
+            <p class="button-project btn-github ">Go to GITHUB</p>
+            </a>
+        </div>
+
+        <div class="work-project-card card-1">
+            <h1 class="work-project-card-title ">
+                Movie Search
+            </h1>
+            <a href="https://djordjevicn.github.io/Movie-Search-API-/" target="_blank">
+            <img class="qr-card" src="./QRprojects/movieSearchQR.jpg"> </a>
+            <a href="https://github.com/DjordjevicN/Movie-Search-API-" target="_blank">  
+            <p class="button-project btn-github ">Go to GITHUB</p>
+            </a>
+        </div>
+
+        
+    </div>
+    <p class="mobile">GO MOBILE</p>
+</div>
+
+</section>`;
+const home = document.querySelector("#home");
+const about = document.querySelector("#about");
+const work = document.querySelector("#work");
+const skills = document.querySelector("#skills");
+let contentBox = document.querySelector(".content-wrapper");
+let phoneContact = document.querySelector(".letsTalk");
+let enter = document.querySelector(".preloader-wrapper");
+
+document.addEventListener("DOMContentLoaded", () => {
+  let enterSequence = new TimelineMax({});
+
+  contentBox.innerHTML = "";
+  contentBox.innerHTML = homeContent;
+  enterSequence
+    .from(".letsTalk", 1, {
+      x: -300
+    })
+    .from("#home", 0.1, {
+      opacity: 0
+    })
+    .from("#home", 0.3, {
+      y: 400,
+      fontSize: 250
+    })
+    .from("#about", 0.1, {
+      opacity: 0
+    })
+    .from("#about", 0.3, {
+      y: 400,
+      fontSize: 250
+    })
+    .from("#work", 0.1, {
+      opacity: 0
+    })
+    .from("#work", 0.3, {
+      y: 400,
+      fontSize: 250
+    })
+    .from("#skills", 0.1, {
+      opacity: 0
+    })
+    .from("#skills", 0.3, {
+      y: 400,
+      fontSize: 250
+    })
+    .from(".home", 1, {
+      opacity: 0,
+      x: -300
+    })
+    .from(".t1", 0.1, {
+      opacity: 0
+    })
+    .from(".t2", 0.1, {
+      opacity: 0
+    })
+    .from(".t3", 0.1, {
+      opacity: 0
+    })
+    .from(".interface-settings li", 0.9, {
+      x: 550
+    });
+
+  home.addEventListener("click", () => {
+    contentBox.innerHTML = "";
+    contentBox.innerHTML = homeContent;
+    let openHome = new TimelineMax({});
+    openHome
+      .from(".home", 1, {
+        opacity: 0,
+        x: -300
+      })
+      .from(".t1", 0.1, {
+        opacity: 0
+      })
+      .from(".t2", 0.1, {
+        opacity: 0
+      })
+      .from(".t3", 0.1, {
+        opacity: 0
+      });
+  });
+  about.addEventListener("click", () => {
+    contentBox.innerHTML = "";
+    contentBox.innerHTML = aboutContent;
+    let openAbout = new TimelineMax({});
+    openAbout
+      .from(".about-section", 1, {
+        opacity: 0,
+        x: 300
+      })
+      .from(
+        ".about-title h1",
+        0.1,
+        {
+          opacity: 0
+        },
+        "ww"
+      )
+      .from(".about-title h1", 0.1, {
+        opacity: 1
+      })
+      .from(".about-title h1", 0.1, {
+        opacity: 0
+      })
+      .from(".about-title h1", 0.1, {
+        opacity: 1
+      })
+      .from(".about-title h1", 0.1, {
+        opacity: 0
+      })
+      .from(".about-text p", 0.2, {
+        opacity: 0
+      });
+  });
+  work.addEventListener("click", () => {
+    contentBox.innerHTML = "";
+    contentBox.innerHTML = workContent;
+    let openWork = new TimelineMax({});
+
+    openWork
+      .from(".work-content-card", 1, {
+        opacity: 0,
+        x: -300
+      })
+      .from(
+        ".card-5",
+        0.1,
+        {
+          opacity: 0
+        },
+        "op"
+      )
+      .from(
+        ".card-2",
+        0.1,
+        {
+          opacity: 0
+        },
+        "op"
+      )
+      .from(
+        ".card-3",
+        0.1,
+        {
+          opacity: 0
+        },
+        "op"
+      )
+      .from(
+        ".card-6",
+        0.1,
+        {
+          opacity: 0
+        },
+        "oz"
+      )
+      .from(
+        ".card-1",
+        0.1,
+        {
+          opacity: 0
+        },
+        "oz"
+      )
+      .from(
+        ".card-4",
+        0.1,
+        {
+          opacity: 0
+        },
+        "oz"
+      );
+
+    let goMobile = document.querySelector(".mobile");
+    goMobile.addEventListener("click", () => {
+      contentBox.innerHTML = "";
+      contentBox.innerHTML = mobileContent;
+    });
+  });
+
+  skills.addEventListener("click", () => {
+    contentBox.innerHTML = "";
+    contentBox.innerHTML = skillsContent;
+    let openSkills = new TimelineMax({});
+    openSkills
+      .from(".skills-content", 0.6, {
+        opacity: 0,
+        x: 300
+      })
+      .from(
+        ".skills-title h1",
+        0.1,
+        {
+          opacity: 0
+        },
+        "ww"
+      )
+      .from(".skills-title h1", 0.1, {
+        opacity: 1
+      })
+      .from(".skills-title h1", 0.1, {
+        opacity: 0
+      })
+      .from(".skills-title h1", 0.1, {
+        opacity: 1
+      })
+      .from(".skills-title h1", 0.1, {
+        opacity: 0
+      })
+      .from(
+        ".skills-text",
+        0.2,
+        {
+          opacity: 0
+        },
+        "ww",
+        1
+      );
+  });
+  let openContact = new TimelineMax({ paused: true, reversed: true });
+  phoneContact.addEventListener("click", () => {
+    phoneContact.classList.toggle("opened");
+
+    openContact.reversed() ? openContact.play() : openContact.reverse();
+
+    openContact.to(".interface-settings li", 0.3, {
+      x: 550
+    });
+  });
+});
